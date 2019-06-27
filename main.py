0432ea53cf5662c8d435569276f43a1e09abd4b1
Christopher Alex Chable <calexchable@gmail.com>
2019-06-27 08:09:21 -0500

    added sign in form, calex

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <style>
-            .error { color: red; }
-        </style>
-    </head>
-    <body>
-        <h1>User Sign-up</h1>
-        <form method="POST">
-            <label for="username">Username:</label>
-            <input name="username" type="text" value="" />
-            <label for="password">Password:</label>
-            <input name="password" type="password" value="" />
-            <label for="verify">Verify Password:</label>
-            <input for="verify" type="password" value="" />
-            <label for="email">Email (optional):</label>
-            <input for="email" type="text" value="" />
-            <input type="submit" value="Submit" />
-        </form>
-    </body>
-</html>
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/")
+def index():
+    return render_template("sign_in_form.html")
