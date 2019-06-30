ece3ea2e40341ea16b53c697f9b4c543f8f6c9c2
Christopher Alex Chable <calexchable@gmail.com>
2019-06-30 12:56:40 -0500

    added if statements, calex

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 import cgi
+import os
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -9,14 +10,61 @@ def index():
     return render_template("sign_in_form.html")
 
 @app.route("/", methods=["POST"])
-def welcome():
+
+def sign_in():
     username = request.form["username"]
     password = request.form["password"]
     verify = request.form["verify"]
     email = request.form["email"]
 
+    username_error = ""
+    password_error = ""
+    verify_error = ""
+    email_error = ""
 
+    # User name validation #
+    if username == "":
+        username_error = "Please enter a username."
+
+    elif len(username) < 3 or len(username) > 20:
+        username_error = "Username must contain between 3 and 20 characters."
+        username = ""
     
-    return render_template("welcome.html", username=username)
+    elif " " in username:
+        username_error = "Username cannot contain any spaces."
+
+    # Validate password #
+    if password == "":
+        password_error = "Please enter a valid password."
+    
+    elif len(password) < 3 or len(password) > 20:
+        password_error = "Password must contain between 3 and 20 characters."
+        password = ""
+    
+    elif " " in password:
+        password_error = "Password must not contain spaces."
+        password = ""
+    
+    # Password verification #
+    if verify == "":
+        verify_error = "Please validate your password."
+    
+    elif verify != password:
+        verify_error = "Passwords do not mach. Please re-enter password."
+        verify = ""
+    
+    # Validate Email #
+    # ???? #
+
+    if not username_error and not password_error and not verify_error and not email_error:
+        username = username
+        return render_template("welcome.html", username = username)
+    else: return render_template("sign_in_form.html",
+        username = username,
+        username_error = username_error,
+        password_error = password_error,
+        email = email,
+        email_error = email_error
+        )
 
 app.run()
